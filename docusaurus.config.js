ab4869dff82c85aa5fb80a96a4467981fa1e5bf5
Mouhamed Mboup <amethmboup99@gmail.com>
2024-11-20 12:29:55 +0000

    ready to deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,16 @@ const config = {
   favicon: 'img/blockchain.avif',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/kumulus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'MouhamedMboup', // Usually your GitHub org/user name.
+  projectName: 'Kumulus', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
